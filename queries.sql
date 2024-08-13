6179301de054954371175480da1de18c1f84f40a
Julia Samozhenova <72604628+vishnevoe@users.noreply.github.com>
2024-08-13 21:01:00 +0300

    add new files, update queries
    add age_groups.csv, customers_by_month.csv, special_offer.csv, update queries.sql

--- queries.sql
+++ queries.sql
@@ -51,3 +51,44 @@ left join products p
 	on s.product_id = p.product_id
 group by seller, s.sale_date
 order by extract(isodow from s.sale_date), seller;
+
+# Запрос, с помощью которого формируется отчет с информацией о количестве пользователей в трех возрастных группах: 16-25, 26-40, старше 40. Данные отсортированы по возрастным группам.
+
+select 
+	case 
+		when age between 16 and 25 then '16-25'
+		when age between 26 and 40 then '26-40'
+		when age > 40 then '40+'
+	end as age_category,
+	COUNT(*) as age_count
+from customers 
+group by age_category
+order by age_category;
+
+# Запрос, с помощью которого формируется отчет с информацией о количестве уникальных покупателей и выручке, которую они принесли. Данные сгруппированы по дате и отсортированы по дате по возрастанию.
+
+select
+	TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+	COUNT(distinct s.customer_id) as total_customers,
+	SUM(floor(s.quantity * p.price)) as income
+from sales s
+left join products p 
+	on s.product_id = p.product_id
+group by TO_CHAR(s.sale_date, 'YYYY-MM')
+order by selling_month;
+
+# Запрос, с помощью которого формируется отчет о покупателях, первая покупка которых была в ходе проведения акций (цена товара указана как равная нулю). Данные отсортированы по id покупателя.
+
+select DISTINCT ON (s.customer_id)
+	concat(c.first_name, ' ', c.last_name) as customer,
+	s.sale_date as sale_date,
+	concat(e.first_name, ' ', e.last_name) as seller
+from sales s
+left join employees e 
+	on s.sales_person_id = e.employee_id
+left join customers c
+	on s.customer_id = c.customer_id
+left join products p
+	on s.product_id = p.product_id
+where p.price = 0 
+order by s.customer_id, s.sale_date;
